f1a2856f99d6018f6af6ccdbd26315bfa4ec24d2
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-10-10 11:15:47 +0100

    Added comments for recursive factorial solution

--- Recursion.java
+++ Recursion.java
@@ -1,6 +1,13 @@
 public class Recursion {
-    
-    
+
+    /**
+     * Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+     * factorial(1) → 1
+     * factorial(2) → 2
+     * factorial(3) → 6
+     * @param n  
+     * @return the sum of the multiplication of n times n - 1 until n equals 0
+     */ 
     public int factorial(int n) {
 
         //base case
